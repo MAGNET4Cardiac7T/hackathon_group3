faf049a56e44e543f627606517215c55454df682
Andrzej Dulny <58820243+badulion@users.noreply.github.com>
2025-04-11 00:34:09 +0200

    Fix for evaluation

--- src/costs/__init__.py
+++ src/costs/__init__.py
@@ -1,4 +1,4 @@
 from .b1_homogeneity import B1HomogeneityCost
-from .b1_sar_homogeneity import B1SARHomogeneityCost
+from .b1_sar_homogeneity import B1SARHomogeneityCost as B1HomogeneitySARCost
 from .b1_homogeneity_minmax import B1HomogeneityMinMaxCost
 from .b1_sar_homogeneity_masked_borders import masked_borders_B1SARHomogeneityCost
